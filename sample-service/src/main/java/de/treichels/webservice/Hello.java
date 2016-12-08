1fd6638bdb3c00f83118771ca795c76e52af7809
Oliver Treichel <oliver.treichel@gmx.de>
2016-12-08 21:58:41 +0100

    load greeting from properties file

--- sample-service/src/main/java/de/treichels/webservice/Hello.java
+++ sample-service/src/main/java/de/treichels/webservice/Hello.java
@@ -1,17 +1,34 @@
 package de.treichels.webservice;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
 
 @WebService
 public class Hello {
-	public static void main(final String[] args) {
-		Endpoint.publish("http://localhost:8080/ws/hello", new Hello());
+	private static final Properties PROPS = new Properties();
+
+	static {
+		try (final InputStream is = Hello.class.getResourceAsStream("/config.properties")) {
+			if (is != null) {
+				PROPS.load(is);
+			}
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void main(final String[] args) throws IOException {
+		PROPS.setProperty("greeting", "Hello, World!");
+		Endpoint.publish("http://localhost:8080/ws", new Hello());
 	}
 
 	@WebMethod
-	public String sayHello() {
-		return "Hello, World!";
+	public String sayHello() throws IOException {
+		return PROPS.getProperty("greeting");
 	}
 }
