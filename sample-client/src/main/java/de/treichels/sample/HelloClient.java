b94fd484ae3a068271773a355336a5b5f69d9194
Oliver Treichel <oliver.treichel@gmx.de>
2016-12-08 22:36:24 +0100

    handle IOException from server

--- sample-client/src/main/java/de/treichels/sample/HelloClient.java
+++ sample-client/src/main/java/de/treichels/sample/HelloClient.java
@@ -5,16 +5,17 @@ import java.net.URL;
 
 import de.treichels.webservice.Hello;
 import de.treichels.webservice.HelloService;
+import de.treichels.webservice.IOException_Exception;
 
 public class HelloClient {
-	public static void callService(final URL wsdl) {
+	public static void callService(final URL wsdl) throws IOException_Exception {
 		final HelloService service = new HelloService(wsdl);
 		final Hello hello = service.getHelloPort();
 
 		System.out.println(hello.sayHello());
 	}
 
-	public static void main(final String[] args) throws MalformedURLException {
+	public static void main(final String[] args) throws MalformedURLException, IOException_Exception {
 		callService(new URL("http://localhost:8080/dev/ws?wsdl"));
 		callService(new URL("http://localhost:8080/qa/ws?wsdl"));
 		callService(new URL("http://localhost:8080/prod/ws?wsdl"));
