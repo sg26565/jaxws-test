1856704d28f691480a6be1b11f95e632066f5d81
Oliver Treichel <oliver.treichel@gmx.de>
2016-12-08 22:01:54 +0100

    call dev, qa and prod environment

--- sample-client/src/main/java/de/treichels/sample/HelloClient.java
+++ sample-client/src/main/java/de/treichels/sample/HelloClient.java
@@ -7,10 +7,16 @@ import de.treichels.webservice.Hello;
 import de.treichels.webservice.HelloService;
 
 public class HelloClient {
-	public static void main(final String[] args) throws MalformedURLException {
-		final HelloService service = new HelloService(new URL("http://localhost:8080/ws/hello?wsdl"));
+	public static void callService(final URL wsdl) {
+		final HelloService service = new HelloService(wsdl);
 		final Hello hello = service.getHelloPort();
 
-		System.out.print(hello.sayHello());
+		System.out.println(hello.sayHello());
+	}
+
+	public static void main(final String[] args) throws MalformedURLException {
+		callService(new URL("http://localhost:8080/dev/ws?wsdl"));
+		callService(new URL("http://localhost:8080/qa/ws?wsdl"));
+		callService(new URL("http://localhost:8080/prod/ws?wsdl"));
 	}
 }
